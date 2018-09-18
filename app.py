6cf0719049973b0b175cf5278b8c107071799c06
Sumit Rastogi <mail.sumitrastogi@gmail.com>
2018-09-19 01:53:06 +0530

    corrected app name in app.py

--- app.py
+++ app.py
@@ -76,16 +76,23 @@ def report():
     data.append(['Profit/Loss',str((sum(value)-sum(cost))*100/sum(cost))+"%",'',sum(cost),sum(value),sum(value)-sum(cost)])
     return render_template('report.html',mf_data=data)
 
+def save_nav_in_db(NAV):
+    conn = connect_db('MF.db')
+    conn.execute("INSERT INTO MF_RECORD (DATE, MF1, MF2, MF3, MF4, MF5, MF6, MF7) VALUES(?,?,?,?,?,?,?,?)",tuple(NAV))
+    print("Record Saved")
+    conn.commit()
+    conn.close()
+
 def read_nav_from_internet():
     MF_NAV = [str(datetime.datetime.now())]
     MF_DICT = {
-                 'MSB079': r'http://www.moneycontrol.com/mutual-funds/nav/sbi-blue-chip-fund/MSB079',
-                 'MBS291': r'http://www.moneycontrol.com/mutual-funds/nav/birla-sun-life-tax-relief-96/MBS291',
-                 'MTE117': r'http://www.moneycontrol.com/mutual-funds/nav/franklin-india-high-growth-companies-fund/MTE117', 
-                 'MKP002' : r'http://www.moneycontrol.com/mutual-funds/nav/franklin-india-prima-fund/MKP002', 
-                 'MPI1116' : r'http://www.moneycontrol.com/mutual-funds/nav/icici-prudential-value-discovery-fund-direct-plan/MPI1116', 
-                 'MKM311' : r'http://www.moneycontrol.com/mutual-funds/nav/kotak-select-focus-fund-regular-plan/MKM311', 
-                 'MMA100' : r'http://www.moneycontrol.com/mutual-funds/nav/mirae-asset-emerging-bluechip-fund-direct-plan/MMA100'}
+                 'MSB079': r'https://www.moneycontrol.com/mutual-funds/nav/sbi-blue-chip-fund/MSB079',
+                 'MBS291': r'https://www.moneycontrol.com/mutual-funds/nav/birla-sun-life-tax-relief-96/MBS291',
+                 'MTE117': r'https://www.moneycontrol.com/mutual-funds/nav/franklin-india-high-growth-companies-fund/MTE117', 
+                 'MKP002' : r'https://www.moneycontrol.com/mutual-funds/nav/franklin-india-prima-fund/MKP002', 
+                 'MPI1116' : r'https://www.moneycontrol.com/mutual-funds/nav/icici-prudential-value-discovery-fund-direct-plan/MPI1116', 
+                 'MKM311' : r'https://www.moneycontrol.com/mutual-funds/nav/kotak-select-focus-fund-regular-plan/MKM311', 
+                 'MMA100' : r'https://www.moneycontrol.com/mutual-funds/nav/mirae-asset-emerging-bluechip-fund-direct-plan/MMA100'}
     
     for MF in MF_DICT:
         try:
@@ -95,12 +102,6 @@ def read_nav_from_internet():
         except:
             print("Please Rerun.. Values are not in correct order")
     return MF_NAV
-
-def save_nav_in_db(NAV):
-    conn = connect_db('MF.db')
-    conn.execute("INSERT INTO MF_RECORD (DATE, MF1, MF2, MF3, MF4, MF5, MF6, MF7) VALUES(?,?,?,?,?,?,?,?)",tuple(NAV))
-    conn.commit()
-    conn.close()
 
 if __name__ == '__main__':
     app.run(debug = False)
